48ad33a6e833a24d67e0aa39bddeb3e9f82ad49c
tridev09-ops <tridev17848@example.com>
2025-08-31 17:16:10 +0530

    added frontend

---
+++ static/script.js
@@ -0,0 +1,43 @@
+const input = document.getElementById("fileInput");
+const size = document.getElementById("size");
+const form = document.getElementById("compressForm");
+const qualityDropdown= document.getElementById("qualityDropdown");
+
+let orderedFiles = [];
+
+input.addEventListener("change", (event) => {
+    const files = Array.from(event.target.files);
+    orderedFiles = []; // reset
+
+    files.forEach((file, i) => {
+        orderedFiles.push(file);
+    });
+});
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+
+    orderedFiles.forEach(file => {
+        formData.append("files", file);
+    });
+
+    formData.append("size", parseInt(size.value))
+    
+    formData.append("quality", qualityDropdown.value)
+    
+    fetch("/compress",
+        {
+            method: "POST",
+            body: formData
+        })
+    .then(res => res.blob())
+    .then(blob => {
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = "";
+        a.click();
+        window.URL.revokeObjectURL(url);
+    });
+});
