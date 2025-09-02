6bd9df791e4ab07d9ae01b945d998c5e3f3dc19b
tridev09-ops <tridev17848@example.com>
2025-09-02 14:02:15 +0530

    added dark mode

--- static/script.js
+++ static/script.js
@@ -7,10 +7,15 @@ const compressInput = document.querySelector("#compressForm #fileInput")
 const size = document.querySelector("#size")
 const quality = document.querySelector("#quality")
 
-const toggle = document.querySelector(".toggle")
 const convertBtn = document.querySelector("#convertBtn")
 const compressBtn = document.querySelector("#compressBtn")
 
+const responseDiv = document.querySelector("#responseDiv")
+const loaderCon = document.querySelector("#loader-container")
+
+const toggleMode = document.querySelector("#toggle")
+
+let darkMode = false
 let orderedFiles = []
 let exts = []
 const image_ext = ['jpg', 'png', 'jpeg']
@@ -24,6 +29,12 @@ const reset = ()=> {
     ext.style.display = 'none'
     convertForm.style.display = 'none'
     compressForm.style.display = 'none'
+
+    convertInput.value = ''
+    compressInput.value = ''
+
+    responseDiv.innerHTML = ''
+    loaderCon.style.display = 'none'
 }
 
 reset()
@@ -31,8 +42,9 @@ convertForm.style.display = ''
 
 const appendFiles = (event) => {
     const files = Array.from(event.target.files)
-    orderedFiles = [] // reset
+    orderedFiles = []
     exts = []
+    responseDiv.innerHTML = ''
 
     files.forEach((file, i) => {
         orderedFiles.push(file)
@@ -58,7 +70,7 @@ const appendFiles = (event) => {
     }
 }
 
-const submitForm = (event, route)=> {
+const submitForm = async(event, route)=> {
     event.preventDefault()
     const formData = new FormData()
 
@@ -67,30 +79,65 @@ const submitForm = (event, route)=> {
     })
 
     if (route == "/convert") {
+        if (!ext.value) {
+            alert("Select extension to compress")
+            return
+        }
         formData.append("ext", ext.value)
     } else if (route == "/compress") {
         if (image_ext.includes(exts[0])) {
+            if (!size.value) {
+                alert("Enter size to compress")
+                return
+            }
             formData.append("size", parseInt(size.value))
         } else if (video_ext.includes(exts[0])) {
+            if (!quality.value) {
+                alert("Select quality to compress")
+                return
+            }
             formData.append("quality", quality.value)
         }
     }
+
+    loaderCon.style.display = 'grid'
 
     fetch(route,
         {
             method: "POST",
             body: formData
         })
-    .then(res => res.blob())
-    .then(blob => {
-        const url = window.URL.createObjectURL(blob)
-        const a = document.createElement("a")
-        a.href = url
-        a.download = ""
-        a.click()
-        window.URL.revokeObjectURL(url)
+    .then(res => res.json())
+    .then(async (data) => {
+        const btn = document.createElement("button")
+        btn.textContent = "Download"
+        responseDiv.appendChild(btn)
+
+        btn.addEventListener("click", async ()=> {
+            await data.forEach((url)=> {
+                const a = document.createElement("a")
+                a.href = url
+                a.download = ""
+                a.click()
+            })
+
+            fetch('/delete')
+        })
+
+        loaderCon.style.display = 'none'
     })
 }
+
+toggleMode.addEventListener("change", ()=> {
+    if (!darkMode) {
+        document.body.classList.add('dark')
+        darkMode=!darkMode
+    } else {
+        document.body.classList.remove('dark')
+        darkMode=!darkMode
+    }
+
+})
 
 convertInput.addEventListener("change", (event) => {
     appendFiles(event)
